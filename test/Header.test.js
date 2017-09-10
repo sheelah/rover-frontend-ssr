7d3e45a834db76903bf339327bb7be70127fbe44
Sheelah Brennan <slb53@hotmail.com>
2017-09-09 22:03:32 -0700

    Add missing semicolon

--- test/Header.test.js
+++ test/Header.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import renderer from 'react-test-renderer'
+import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import Header from '../components/Header.js';
 
